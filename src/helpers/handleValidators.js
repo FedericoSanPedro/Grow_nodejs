3293697d7101bd3ad2c5f86e970ffc776ea9d0ce
Fede <federico.sanpedro@avalith.net>
2022-12-07 15:23:17 -0300

    validateRole funcionaaaaaa

--- src/helpers/handleValidators.js
+++ src/helpers/handleValidators.js
@@ -13,14 +13,12 @@ handleValidators.validateResults = (req, res, next) => {
 };
 
 handleValidators.validateRole = (role, req, res, next) => {
-    try {
-        if(typeof req.userInfo.role != 'undefined' && req.userInfo.role == role){
+
+        if(typeof req.user.role != 'undefined' && req.user.role == role){
             return next();
         }
-    } catch (error) {
+
         res.status(403).json({error: 'No puedes hacer usar este route con tu tipo de user'});
-        res.send({errors: error.array()});
-    }
 };
 
 export { handleValidators };
